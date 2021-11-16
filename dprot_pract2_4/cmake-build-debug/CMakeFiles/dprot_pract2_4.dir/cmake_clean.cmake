503520406853c5148f0f1966196514fe219ee50e
Manel Gracia <manel.gracia.sola@gmail.com>
2021-11-16 17:55:55 +0100

    Refactor

---
+++ dprot_pract2_4/cmake-build-debug/CMakeFiles/dprot_pract2_4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dprot_pract2_4.dir/main.c.o"
+  "dprot_pract2_4"
+  "dprot_pract2_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dprot_pract2_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dprot_pract2_5/cmake-build-debug/CMakeFiles/dprot_pract2_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dprot_pract2_5.dir/main.c.o"
+  "dprot_pract2_5"
+  "dprot_pract2_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dprot_pract2_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
